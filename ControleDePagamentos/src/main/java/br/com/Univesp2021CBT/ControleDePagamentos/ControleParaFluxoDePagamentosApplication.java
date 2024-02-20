8f4853017098484319695f905d424d822fc4cb23
LeandroLDA <45002731+LeandroLDA@users.noreply.github.com>
2024-02-20 11:21:52 -0300

    Carregado projeto maven

---
+++ ControleDePagamentos/src/main/java/br/com/Univesp2021CBT/ControleDePagamentos/ControleParaFluxoDePagamentosApplication.java
@@ -0,0 +1,13 @@
+package br.com.Univesp2021CBT.ControleDePagamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleParaFluxoDePagamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleParaFluxoDePagamentosApplication.class, args);
+	}
+
+}
