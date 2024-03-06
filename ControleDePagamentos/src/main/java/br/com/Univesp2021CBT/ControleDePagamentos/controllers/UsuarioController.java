4877a60a93204d0b8e0d12f4d8855e42c9eaa5ea
LeandroLDA <45002731+LeandroLDA@users.noreply.github.com>
2024-03-05 22:32:17 -0300

    Concluido controller de Usuário

--- ControleDePagamentos/src/main/java/br/com/Univesp2021CBT/ControleDePagamentos/controllers/UsuarioController.java
+++ ControleDePagamentos/src/main/java/br/com/Univesp2021CBT/ControleDePagamentos/controllers/UsuarioController.java
@@ -1,17 +1,17 @@
 package br.com.Univesp2021CBT.ControleDePagamentos.controllers;
 
-import br.com.Univesp2021CBT.ControleDePagamentos.Usuario.DadosAtualizaUsuario;
-import br.com.Univesp2021CBT.ControleDePagamentos.Usuario.DadosCadastroUsuario;
-import br.com.Univesp2021CBT.ControleDePagamentos.Usuario.Usuario;
-import br.com.Univesp2021CBT.ControleDePagamentos.Usuario.UsuarioRepository;
-import jakarta.persistence.Entity;
+import br.com.Univesp2021CBT.ControleDePagamentos.Usuario.*;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/usuarios") @Entity
+@RequestMapping("/usuarios")
+
 public class UsuarioController {
 
     @Autowired
@@ -24,6 +24,10 @@ public class UsuarioController {
         repository.save(new Usuario(dados));
     }
 
+    @GetMapping
+    public Page<DadosListagemUsuario> listar (@PageableDefault(size =10, sort = {"nome"})Pageable paginacao){
+        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemUsuario::new);
+    }
     @PutMapping
     @Transactional
     public void atualizar (@RequestBody @Valid DadosAtualizaUsuario dados){
@@ -34,8 +38,8 @@ public class UsuarioController {
     @DeleteMapping("/{id}")
     @Transactional
     public void excluir (@PathVariable Long id){
-        var usuario = repository.getReferenceById(dados.id());
-        usuario.excluir(dados);
+        var usuario = repository.getReferenceById(id);
+        usuario.excluir();
 
     }
 }
